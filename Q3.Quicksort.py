dc5d0fe3a2decb9b74a265ad7ee763a4bf26183e
Ramsundhar-88 <Ramsundhar.sades@gmail.com>
2025-12-22 16:37:40 +0530

    Completed Quick sort

---
+++ Q3.Quicksort.py
@@ -0,0 +1,20 @@
+def quickSort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quickSort(arr, low, pivot_index - 1)
+        quickSort(arr, pivot_index + 1, high)
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+arr = [10, 7, 8, 9, 1, 5]
+print("Original:", arr)
+quickSort(arr, 0, len(arr) - 1)
+print("Sorted:  ", arr)
